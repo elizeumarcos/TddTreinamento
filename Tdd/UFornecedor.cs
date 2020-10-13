eca6560aedfec95912f53c459935f6c2c66e6264
elizeumarcos <elizeubarros@gmail.com>
2020-10-13 20:51:44 -0300

    Teste classe Fornecedor

---
+++ Tdd/UFornecedor.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using TddConsole.Models;
+
+namespace Tdd
+{
+    public class UFornecedor
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+        [Test]
+        public void ValidaMetodoStaticDeUmList()
+        {
+            Assert.IsNotNull(Fornecedor.Todos());
+
+        }
+
+    }
+
+}
---
+++ TddConsole/Models/Fornecedor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TddConsole.Models
+{
+    public class Fornecedor
+    {
+        public static List<object> Todos()
+        {
+            var _fornecedor = new List<object>();
+            return _fornecedor;
+        }
+    }
+}
